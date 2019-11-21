fccf0db1aa3921d8e5995ad8fe3fc71838d62db1
Ninibeth Teixeira <ninibethreina@gmail.com>
2019-11-21 16:22:30 +0000

    Done.

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -19,19 +19,33 @@ ENFIELD_TENNIS_ACADEMY = [
 def assembled_matrix
   # Build an array that contains (or, "nests") the residents of The Ennet House
   # and the Enfield Tennis Academy as provided by the constants
+Ennet_House = ["Don Gately", "Joelle Van Dyne", "Pat M.", "Kate Gompert", "Bruce Green"]
+Enfiled_Tennis_Academy = ["Hal Incandenza", "Lyle", "Gerhard Schtitt", "Mario Incandenza", "Michael Pemulis"]
+new_array= [ENNET_HOUSE, ENFIELD_TENNIS_ACADEMY]
+pp new_array 
 end
+assembled_matrix 
+
 
 def array_literal_matrix
   # Using Array literal syntax only, build a nested array that uses the data in
   # side the ENNET_HOUSE and ENFIELD_TENNIS_ACADEMY Arrays but sorts
   # alphabetically by the first character.
+  Nested_array= [
+    ENFIELD_TENNIS_ACADEMY,
+    ENNET_HOUSE]
+    Nested_array.sort 
 end
 
-def matrix_lookup(matrix, row, column)
+def matrix_lookup(Nested_array, 0, 1)
   # Return the matrix's content at row and column
+Nested_array [0][1]
+pp Nested_array
 end
 
-def matrix_update(matrix, row, column, new_value)
+def matrix_update(Nested_array, 0, 1, Jennifer)
   # Update the matrix location at row and column to have the value of new_value
   # Return the updated matrix
+  Nested_array [0][1]= Jennifer 
+  pp Nested_array 
 end
